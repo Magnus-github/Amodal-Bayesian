237e1c8b3941098f069d30ef7ee30d4a95fe4215
Magnus Tibbe <tibbe@moria.rpl>
2023-09-18 15:46:25 +0000

    Job is running, now need to figure out accessing files/images. Also some problem with conda envs

--- Code/testing.py
+++ Code/testing.py
@@ -8,13 +8,14 @@ DATA_FOLDER = "/local_storage/users/tibbe/Dataset/kitti/"
 
 def main():
     print("Hello World!!!")
-    img_file = DATA_FOLDER + "training/images/000001.jpg"
+    img_file = DATA_FOLDER + "training/images/000001.png"
     img = Image.open(img_file)
     transform = transforms.Compose([
         transforms.Resize(256,256),
         transforms.ToTensor()
     ])
     img_tensor = transform(img)
+    print("DONE!")
 
 if __name__ == "__main__":
     main()
